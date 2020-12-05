12fe0d161253419bb271d8e987698ed0718e9f49
Alexandra <35376766+AlexandraConnangle@users.noreply.github.com>
2020-12-05 21:32:08 +0100

    Update navigation

--- cv/php/header/nav.php
+++ cv/php/header/nav.php
@@ -4,7 +4,7 @@
         <div id="menu">
             <div id="tall-menu-link">
                 <ul>
-                    <li><a href="#" data-scroll="presentation">Présentation</a></li>
+                    <!-- <li><a href="#" data-scroll="presentation">Présentation</a></li> -->
                     <li><a href="#" data-scroll="ability">Compétences</a></li>
                     <li><a href="#" data-scroll="portfolio">Réalisations</a></li>
                 </ul>
